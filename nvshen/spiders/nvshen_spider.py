c8984eff4ecf353c2af5d015dccdc843662ba179
lpxzz <lpxzz@hotmail.com>
2020-07-06 18:36:29 +0800

    Update nvshen_spider.py

--- nvshen/spiders/nvshen_spider.py
+++ nvshen/spiders/nvshen_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-import os,sys
+import os
 import re
 import urllib.request
 import time
@@ -8,7 +8,7 @@ class nvshen(scrapy.Spider):
 
     uri = input("请输入网址：")
     name = "nvshen"
-    allowed_domains = ["www.nvshens.net","t1.onvshen.com","t1.onvshen.com:85","nvshens.net"]
+    allowed_domains = ["www.nvshens.net", "t1.onvshen.com", "t1.onvshen.com:85", "nvshens.net"]
     start_urls = [
         uri,
     ]
@@ -16,33 +16,32 @@ class nvshen(scrapy.Spider):
     #选择器
     def parse(self, response):
         yield {"name": response.xpath('//*[@id="htilte"]/text()').extract()} #标题
-        #yield {"description": response.xpath('//*[@id="ddesc"]/text()').extract()} #描述
         imgs =  response.xpath('//*[@id="hgallery"]/img/@src[/]').getall() #图片组
         for img in imgs:
-            self.createDir(response.xpath('//*[@id="htilte"]/text()').extract(),img)
+            self.createDir(response.xpath('//*[@id="htilte"]/text()').extract(), img)
         nextUrl = response.css(".a1::attr(href)").extract()
         num = nextUrl[1].find("html")
-        if num <= 0 :
+        if num <= 0:
             print("不存在下一页,爬取完成")
-        else :
+        else:
             print("存在下一页,继续爬取")
             yield scrapy.Request("https://www.nvshens.net/"+nextUrl[1], self.parse)
 
     #创建文件夹
-    def createDir(self,title,img):
+    def createDir(self, title, img):
         dirName = img.split('/')[5]
-        path = os.path.join(os.getcwd(),dirName)
-        if os.path.isdir(path) :
+        path = os.path.join(os.getcwd(), dirName)
+        if os.path.isdir(path):
             #存在文件夹 下载图片
-            self.downloadImg(path,img)
-        else : 
+            self.downloadImg(path, img)
+        else: 
             #不存在文件夹，创建
             os.makedirs(path)
-            self.downloadImg(path,img)
+            self.downloadImg(path, img)
             
     #下载图片
-    def downloadImg(self,path,url):
+    def downloadImg(self, path, url):
         fileName = url.split('/')[7] #文件名
         i = time.time()
-        urllib.request.urlretrieve(url , path+'/'+fileName)
+        urllib.request.urlretrieve(url, path+'/'+fileName)
         print('下载成功,耗时：'+str(int(time.time() - i))+'s')
